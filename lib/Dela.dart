4b677d479eb5c664538bbb67aff5e6be0783e7bf
DelaSafitri <delaodet9@gmail.com>
2022-05-15 18:51:56 +0700

    tambahan aplikasi dari dela

---
+++ lib/Dela.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class dela extends StatefulWidget {
+  const dela({Key? key}) : super(key: key);
+
+  @override
+  State<dela> createState() => _delaState();
+}
+
+class _delaState extends State<dela> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
