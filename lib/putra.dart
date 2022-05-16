38851a0e046df5ba1693c9456c5a4982954bf3c9
mikhsan21 <mikhsannugraha39@gmail.com>
2022-05-16 10:39:57 +0700

    perubahan ihsan

---
+++ lib/putra.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class putra extends StatefulWidget {
+  const putra({Key? key}) : super(key: key);
+
+  @override
+  State<putra> createState() => _putraState();
+}
+
+class _putraState extends State<putra> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
