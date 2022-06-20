12748f53ccc90bd36aaa6f82a43205d2f181b6a9
TegarSubianto <tegarsubianto989@gmail.com>
2022-06-20 12:54:40 +0700

    Menambahkan tampilan pada menu rekomendasi menggunakan listview

--- lib/Widgets/rekomendasi.dart
+++ lib/Widgets/rekomendasi.dart
@@ -1,41 +1,60 @@
 import 'package:flutter/material.dart';
 
 class Rekomendasi extends StatefulWidget {
+  const Rekomendasi({Key? key}) : super(key: key);
+
   @override
-  _RekomendasiState createState() => _RekomendasiState();
+  State<Rekomendasi> createState() => _RekomendasiState();
 }
 
 class _RekomendasiState extends State<Rekomendasi> {
   @override
   Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Rekomendasi',
+      home: HalamanDesain(),
+    );
+  }
+}
+class HalamanDesain extends StatefulWidget {
+  const HalamanDesain({Key? key}) : super(key: key);
+
+  @override
+  State<HalamanDesain> createState() => _HalamanDesainState();
+}
+
+class _HalamanDesainState extends State<HalamanDesain> {
+  final List makanan = [
+    'Asam Lambung', 'Kelebihan Gula Darah', 'Darah Tinggi', 'Anemia', 'Asam Urat', 'Kolestrol Tinggi',
+  ];
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.lightGreen,
-        leading: Icon(Icons.arrow_back_ios, color: Colors.white),
-        title: const Text('Rekomendasi'),
+        title: const Text('Healty Food'),
       ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text("Rekomendasi Makanan", style: TextStyle(
-                        color: Colors.amber,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
+      body: ListView.builder(
+          itemCount: makanan.length,
+          itemBuilder: (BuildContext context, index){
+            return Card(
+                color: Colors.white,
+                elevation: 10,
+                shadowColor: Colors.grey,
+                child: ListTile(
+                  title: Text(makanan[index],style: TextStyle(fontSize: 30),),
+                  subtitle: Text('Makanan yang Direkomendasikan'),
+                  trailing: IconButton(
+                      onPressed: (){
+
+                      },
+                      icon: Icon(Icons.search)
+                  ),
+                )
+            );
+          }
       ),
     );
   }
 }
+
