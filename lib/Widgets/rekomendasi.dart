a9eedeaaa32ec2d083f188e2559f34de7ea2a10b
DelaSafitri <delaodet9@gmail.com>
2022-06-20 07:01:31 +0700

    Memperbaiki menu rekomendasi- perubahan dari Dela Safitri

--- lib/Widgets/rekomendasi.dart
+++ lib/Widgets/rekomendasi.dart
@@ -1,35 +1,18 @@
 import 'package:flutter/material.dart';
 
-class HalamanMaterial extends StatefulWidget {
-  const HalamanMaterial({Key? key}) : super(key: key);
-
+class Rekomendasi extends StatefulWidget {
   @override
-  State<HalamanMaterial> createState() => _HalamanMaterialState();
+  _RekomendasiState createState() => _RekomendasiState();
 }
 
-class _HalamanMaterialState extends State<HalamanMaterial> {
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Rekomendasi',
-      home: HalamanDesain(),
-    );
-  }
-}
-class HalamanDesain extends StatefulWidget {
-  const HalamanDesain({Key? key}) : super(key: key);
-
-  @override
-  State<HalamanDesain> createState() => _HalamanDesainState();
-}
-
-class _HalamanDesainState extends State<HalamanDesain> {
+class _RekomendasiState extends State<Rekomendasi> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.lightGreen,
-        title: const Text('Healty Food'),
+        leading: Icon(Icons.arrow_back_ios, color: Colors.white),
+        title: const Text('Rekomendasi'),
       ),
     );
   }
